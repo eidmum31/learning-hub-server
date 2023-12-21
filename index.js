7fb48b7333c9407ed8d6473b2b318e626ae6fd39
eidmum31 <mdzuneadabedineidmum@gmail.com>
2023-12-21 15:25:32 +0600

    all categories route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const cors=require('cors');
 const categories=require("./data/categories.json")
-
+const port=8080;
 
 const app=express();
 app.use(cors());
@@ -11,6 +11,6 @@ app.get('/categories',(req,res)=>{
     res.send(categories);
 })
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("listening at port 8080");
 })
