315f7410c87af660f35f9778db895aa6b73806ec
eidmum31 <mdzuneadabedineidmum@gmail.com>
2023-12-22 11:03:59 +0600

    category wise api added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const cors=require('cors');
-const categories=require("./data/categories.json")
+const categories=require("./data/categories.json");
+const courses=require("./data/courses.json");
 const port=8080;
 
 const app=express();
@@ -9,6 +10,18 @@ app.use(cors());
 app.get('/categories',(req,res)=>{
     console.log(categories);
     res.send(categories);
+})
+
+app.get('/category/:id',(req,res)=>{
+    const id=req.params.id;
+    if(id==0)
+        res.send(courses);
+    else
+    {
+        const categoryCourses=courses.filter(x=>x.category_id==id);
+        res.send(categoryCourses);
+    }
+   
 })
 
 app.listen(port,()=>{
